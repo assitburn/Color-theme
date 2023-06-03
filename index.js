1852d8604e4db8e07e4c20c10c16b5ef3d52c478
assitburn <87494405+assitburn@users.noreply.github.com>
2023-06-03 21:26:40 +0200

    1.0.7

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function readout(){
     "synchronousExecution": false,
   });
   const pinned = result[0].kuiBrightnessLevel._value;
-  
+
   if (pinned == "kPanelBrightnessDarkGray"){
     document.getElementById("btn_dark").innerHTML ='<div slot="icon" class="icon"><svg height="20" viewBox="0 0 20 20" width="20" slot="icon" focusable="false" aria-hidden="true" role="img"><rect x="0" y="0" width="20" height="20" style="fill:#353535;"/><path d="M0.763,17.985l6.95,-9.365l-6.129,-8.605l2.832,0l3.26,4.609c0.678,0.956 1.161,1.692 1.447,2.206c0.4,-0.653 0.874,-1.336 1.421,-2.047l3.616,-4.768l2.587,0l-6.313,8.47l6.803,9.5l-2.942,-0l-4.523,-6.411c-0.253,-0.368 -0.515,-0.768 -0.784,-1.201c-0.401,0.654 -0.687,1.103 -0.858,1.348l-4.511,6.264l-2.856,-0Z" style="fill:#fff;fill-rule:nonzero;"/></svg></div>';  
   }
@@ -86,6 +86,16 @@ async function colorchange_action(theme){
     
   });
 }
+
+document.addEventListener('uxpcommand', (event) => {
+  const { commandId } = event
+  if (commandId === 'uxpshowpanel') {
+    //console.log("panel is showing");
+    auto_run();
+  } else if (commandId === 'uxphidepanel') {
+    //console.log('panel is hiding');
+  }
+})
 
 document.addEventListener("load",auto_run);
 
